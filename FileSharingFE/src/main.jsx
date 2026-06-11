144d956425bb0c730092c9d52bc37ec59181dd48
Sreeraj R <sreerajr342@gmail.com>
2026-06-11 15:41:09 +0530

    Final File Management App with docker file updated

--- FileSharingFE/src/main.jsx
+++ FileSharingFE/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./pages/App.jsx";
 import PublicSharePage from "./pages/PublicSharePage.jsx";
+
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
